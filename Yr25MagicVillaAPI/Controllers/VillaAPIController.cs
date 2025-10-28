50c66382a6ae380fc08989baae1dcfc24b16131e
githubanil2021 <akumar.contact@gmail.com>
2025-10-28 12:20:46 +0530

    Intial Setup and 1st EndPoint

---
+++ Yr25MagicVillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Yr25MagicVillaAPI.Data;
+using Yr25MagicVillaAPI.Models;
+using Yr25MagicVillaAPI.Models.DTO;
+
+namespace Yr25MagicVillaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+         
+        //public IEnumerable<Villa> GetVillas()
+        //{
+        //    return new List<Villa> {
+        //          new Villa { Id=1, Name="PoolView"},
+        //           new Villa { Id = 2, Name = "BeachView" }
+        //    };
+
+        //}
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas()
+        {
+            return VillaStore.villaList;
+
+        }
+
+        [HttpGet("{id:int}")]
+        public VillaDTO GetVilla(int id)
+        {
+            return VillaStore.villaList.FirstOrDefault(u=>u.Id==id);
+
+        }
+    }
+}
---
+++ Yr25MagicVillaAPI/Data/VillaStore.cs
@@ -0,0 +1,16 @@
+﻿using Yr25MagicVillaAPI.Models.DTO;
+
+namespace Yr25MagicVillaAPI.Data
+{
+    public static class VillaStore
+    {
+        public static List<VillaDTO> villaList = new List<VillaDTO> { 
+                
+                   new VillaDTO { Id=1, Name="PoolView2"},
+                   new VillaDTO { Id = 2, Name = "BeachView" }
+            };
+    }
+
+
+    
+}
