a13e22336e9081d268565e27dbe73cbdfcf3719f
githubanil2021 <akumar.contact@gmail.com>
2025-10-28 18:14:07 +0530

    CRUD Functionality

--- Yr25MagicVillaAPI/Controllers/VillaAPIController.cs
+++ Yr25MagicVillaAPI/Controllers/VillaAPIController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Yr25MagicVillaAPI.Data;
 using Yr25MagicVillaAPI.Models;
@@ -20,17 +21,109 @@ namespace Yr25MagicVillaAPI.Controllers
 
         //}
         [HttpGet]
-        public IEnumerable<VillaDTO> GetVillas()
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
         {
-            return VillaStore.villaList;
+            return Ok(VillaStore.villaList);
 
         }
 
-        [HttpGet("{id:int}")]
-        public VillaDTO GetVilla(int id)
+        [HttpGet("{id:int}", Name ="GetVilla")]
+        [ProducesResponseType(200, Type=typeof(VillaDTO))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public ActionResult  GetVilla(int id)
+        { 
+            
+             if (id == 0)
+                return BadRequest();
+
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if (villa == null)
+                return NotFound();
+
+            return Ok(villa);
+
+        }
+
+        [HttpPost]
+        public ActionResult CreateVilla([FromBody]VillaDTO villaDTO)
         {
-            return VillaStore.villaList.FirstOrDefault(u=>u.Id==id);
+            if(!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+             
+            if (villaDTO == null)
+                return BadRequest(villaDTO);
+
+            if(villaDTO.Id>0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            villaDTO.Id= VillaStore.villaList.OrderByDescending(u => u.Id).FirstOrDefault().Id+1;
+            VillaStore.villaList.Add(villaDTO);
+
+ 
+            return CreatedAtRoute("GetVilla", new {id=villaDTO.Id}, villaDTO);
+
+        }
+
+
+        [HttpDelete("{id:int}", Name ="DeleteVilla")]
+        public IActionResult DeleteVilla(int id)
+        {
+            if (id == 0)
+                return BadRequest();
+
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+
+            if (villa == null)
+                return NotFound();
+
+            VillaStore.villaList.Remove(villa);
+            return NoContent();
+
+        }
+
+        [HttpPut("{id:int}", Name = "UpdateVilla")]
+        public IActionResult UpdateVilla(int id, [FromBody]VillaDTO villaDTO)
+        {
+            if(villaDTO==null || id!=villaDTO.Id)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+
+            villa.Name=villaDTO.Name;
+            villa.Occupancy=villaDTO.Occupancy;
+            villa.Sqft=villaDTO.Sqft;
+
+            return NoContent();
+        }
+
+        [HttpPatch("{id:int}", Name = "UpdatePartialVilla")]
+        public IActionResult UpdatePartialVilla(int id, JsonPatchDocument<VillaDTO> patchDTO)
+        {
+            if (patchDTO == null || id ==0)
+            {
+                return BadRequest();
+            }
+
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if (villa == null )
+            {
+                return BadRequest();
+            }
+
+            patchDTO.ApplyTo(villa, ModelState);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            return NoContent();
 
         }
     }
-}
+    }
--- Yr25MagicVillaAPI/Data/VillaStore.cs
+++ Yr25MagicVillaAPI/Data/VillaStore.cs
@@ -6,8 +6,8 @@ namespace Yr25MagicVillaAPI.Data
     {
         public static List<VillaDTO> villaList = new List<VillaDTO> { 
                 
-                   new VillaDTO { Id=1, Name="PoolView2"},
-                   new VillaDTO { Id = 2, Name = "BeachView" }
+                   new VillaDTO { Id=1, Name="PoolView2", Occupancy=1,Sqft=20},
+                   new VillaDTO { Id = 2, Name = "BeachView",Occupancy=2,Sqft=40 }
             };
     }
 
